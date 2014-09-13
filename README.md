a8fb663c9fab0565494ee254676e6a85e4942730
Sameer Sontakey <sameer@bridg.com>
2014-09-13 00:16:45 -0700

    fixing server

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 Bridg-interview-js
 ==================
 
-Some nterview problems for Javascript with a test runner!
+Some interview problems for Javascript with a test runner!
 
 This only works on Chrome (for now)
 
 1. npm install
 2. node index.js
-3. write your solutions in the problems/ directory and watch as the test runner shows you your results!
+3. Open up localhost:3000
+4. write your solutions in the problems/ directory and watch as the test runner shows you your results!
--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-//var express = require('express');
-//var app = express();
-//var http = require('http');
-//var fs = require('fs');
-//
-//app.use(express.static(__dirname + '/'));
-//
-//// Load runner
-//app.get('/', function(req, res){
-//    fs.createReadStream('./runner.html').pipe(res);
-//});
-//
-//app.listen(3000);
-//
-//
+var express = require('express');
+var app = express();
+var http = require('http');
+var fs = require('fs');
 
-var http = require('http'),
-    fs = require('fs');
+app.use(express.static(__dirname + '/'));
 
-http.createServer(function(req, res) {
+// Load runner
+app.get('/', function(req, res){
     fs.createReadStream('./runner.html').pipe(res);
-}).listen(8000);
+});
+
+app.listen(3000);
+
+
